2ca1d7bf3cecfcaa051fe7b4f7d25763c8541bc7
Megastarbot <89236767+Megastarbot@users.noreply.github.com>
2021-09-13 12:31:20 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🔰My bot🔰', url="@MalluSearchBot"
                                 )
                         ]
                     ]
